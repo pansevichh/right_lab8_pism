991651327f3c931f925510bd6831af80fb6c0eb4
Arina <arinakr02@gmail.com>
2022-11-30 00:28:24 +0300

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
                 "4. Reverse all strings. \n" +
                 "5. Search for the substring. \n" +
                 "6. Display collection. \n" +
-                "7. The longest strings. \n" +
                 "8. Exit." ;
 
         int flag = 0;
@@ -91,30 +90,6 @@ public class Main {
                 case (6):
                     for(int i = 0; i<someStrings.size()-1; i++){
                         System.out.println("["+ i + "]" + someStrings.get(i));
-                    }
-                    break;
-                case (7):
-                    ArrayList sortedStrings = new ArrayList();
-                    sortedStrings = someStrings;
-
-                    class StringLengthSort implements Comparator<String>{
-                        @Override
-                        public int compare(String o1, String o2) {
-                            if(o1.length() < o2.length()){
-                                return 1;
-                            }else{
-                                if(o1.length() > o2.length()){
-                                    return -1;
-                                }else{
-                                    return 0;
-                                }
-                            }
-                        }
-                    }
-                    Comparator<String> stringLengthComparator = new StringLengthSort();
-                    Collections.sort(sortedStrings, stringLengthComparator); // sorting
-                    for(Object str : sortedStrings){
-                        System.out.println(str.toString() + " : " + str.toString().length() + " symbols ");
                     }
                     break;
                 case (8):
